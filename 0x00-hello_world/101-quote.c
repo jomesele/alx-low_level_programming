6ec0a2e61d372cb2e7c1f2cc26a35e3e810dd916
jomesele <jomesele@gmail.com>
2022-09-08 16:00:44 -0700

    no puts and printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- main block
+ * Return: 0
+ */
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main(int argc, char** argv)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (0);
+}
