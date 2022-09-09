05cef3b984db7450d3536a5aeba6a072d7fb7e5e
jomesele <jomesele@gmail.com>
2022-09-09 13:22:33 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char alphab;
+
+	for (alphab = 'z'; alphab => 'a'; alphab--)
+		putchar(alphab);
+	putchar('\n');
+
+	return (0);
+}
