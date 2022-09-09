147d957424fff362dc327f6719d7d65c63d321a5
jomesele <jomesele@gmail.com>
2022-09-09 12:47:17 -0700

    lowercase alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char alphab;
+
+	for (alphab = 'a'; alphab <= 'z'; alphab++)
+	{
+		if (alphab != q && alphab !=a)
+			putchar(alphab);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
