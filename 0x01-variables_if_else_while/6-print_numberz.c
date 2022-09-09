c87ceaa7c0fca3360b05a2539c6e2c33b4596cc6
jomesele <jomesele@gmail.com>
2022-09-09 13:14:13 -0700

    print single digit numberz putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		putchar((j % 10) + '0');
+	}
+
+	printf('\n');
+
+	return (0);
+}
