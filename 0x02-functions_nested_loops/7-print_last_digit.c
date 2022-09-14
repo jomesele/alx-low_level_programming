c7c37684292bc8d105bf60538a0b5fa15fb8d1af
jomesele <jomesele@gmail.com>
2022-09-14 04:48:15 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,8 @@
  */
 int print_last_digit(int num)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	if (num < 0)
+		num = num * -1;
+	_putchar((num % 10) + '0');
+	return (num % 10);
 }
