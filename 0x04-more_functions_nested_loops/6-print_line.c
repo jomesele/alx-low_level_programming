6ff2109c2e75d0aff94a1e00293fa6417e155e38
jomesele <jomesele@gmail.com>
2022-09-15 02:43:10 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line using '_'
+ * @n: the number of under score
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	_putchar('\n');
+}
