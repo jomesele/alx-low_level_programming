d3540d7215efa8ceea70b2ed88750d4e26012ce3
jomesele <jomesele@gmail.com>
2022-09-15 03:17:53 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints square using terminal lines and '#' char
+ * @n: the number of # to print in each line
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
