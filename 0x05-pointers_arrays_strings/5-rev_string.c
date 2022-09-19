465d1fa6cd4a0c1994cd72f46ff0229e98f89b71
jomesele <jomesele@gmail.com>
2022-09-19 08:16:59 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,4 +4,24 @@
  * rev_string -reverse string
  * @s: pointer to print the string
  */
+void rev_string(char *s)
+{
+	char *t = s;
+	char n[500];
+	short c = 0;
 
+	while (*s != '\0')
+	{
+		n[c] = *s;
+		s++;
+		c++;
+	}
+	c = 0;
+
+	while (s > t)
+	{
+		s--;
+		*s = n[c];
+		c++;
+	}
+}
