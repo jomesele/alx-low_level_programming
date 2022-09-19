a86f19675b59f27560dd76d7207284e75e93d304
jomesele <jomesele@gmail.com>
2022-09-19 07:13:03 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "pro.h"
+
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+
+	return (i);
+}
