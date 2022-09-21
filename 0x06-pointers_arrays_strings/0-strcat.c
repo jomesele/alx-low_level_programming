cb7c162293028b8849547094324dc15894387e1a
jomesele <jomesele@gmail.com>
2022-09-21 13:35:01 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}
@@ -24,5 +24,5 @@ char *_strcat(char *dest, char *src)
 		j++;
 	}
 	dest[i] = '\0';
-	return dest;
+	return (dest);
 }
