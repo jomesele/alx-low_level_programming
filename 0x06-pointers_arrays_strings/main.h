21aa1de563a85de20cc0f806bbd1dece32582a07
jomesele <jomesele@gmail.com>
2022-09-23 14:23:40 -0700

    indinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -11,6 +12,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
 
