fb2a04cdacb13d1074b777662bd3dbc3c26d599d
jomesele <jomesele@gmail.com>
2022-09-23 13:51:54 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
+ */
+char *cap_string(char *s)
+{
+int count;
+
+/*  scan through string */
+count = 0;
+while (s[count] != 0)
+{/* if next character after count is a char , capitalise it */
+if (s[0] >= 97 && s[0] <= 122)
+{
+s[0] = s[0] - 32;
+}
